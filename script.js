3507dae75ccfacc25031d1dcf20c8e2b05b0a53f
amen <amennouira02@gmail.com>
2024-10-08 16:29:51 +0200

    fenêtre

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-// Exemple : Bouton "Retour en haut"
+// Obtenir les éléments
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// Ouvrir la modale au clic du bouton
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Fermer la modale au clic sur le 'X'
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Fermer la modale en cliquant en dehors du contenu de la modale
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Bouton "Retour en haut"
 document.addEventListener("scroll", function() {
     const button = document.getElementById("backToTop");
     if (window.pageYOffset > 100) {
